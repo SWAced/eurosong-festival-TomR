a0415eddb53d21ff38c4fffc0d1dc994975654e1
Tom Reyniers <62664566+SWAced@users.noreply.github.com>
2021-12-19 15:16:24 +0100

    Add files via upload

--- src/pages/Rankingpage.vue
+++ src/pages/Rankingpage.vue
@@ -10,13 +10,17 @@
             <tr>
                 <th>Ranking</th>
                 <th>Song Title</th>
+                <th>Song Artist</th>
                 <th>Total Points</th>
+                <th>Total %</th>
                 <th>Spotify Link</th>
             </tr>
             <tr v-for="(song, i) in songs" :key="i">
-                <td>{{i + 1}}</td>
+                <td>#{{i + 1}}</td>
                 <td>{{song.title}}</td>
+                <th>{{song.artist}}</th>
                 <td>{{song.totalPoints}}</td>
+                <td>{{song.percentage}}%</td>
                 <td><iframe :src="song.spotify" height="80" width="100%" frameBorder="0"></iframe></td>
             </tr>
         </table>
@@ -31,7 +35,7 @@
                 songs: []
             }
         },
-                mounted(){
+        mounted(){
             this.fetchSongs();
         },
         methods: {
@@ -49,9 +53,25 @@
                     .then((songs) => {
                         //call fetchVotes method to add totalPoints to songs
                         this.fetchVotes(songs);
-                    });
+                        this.fetchArtists(songs);
+                    })
             },
 
+            fetchArtists(songs) {
+                const url = "http://webservies.be/eurosong/Artists";
+                fetch(url)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((artists) => {
+                        songs.map(song => {
+                            const a = artists.find((artist) => artist.id == song.artist);
+                            song.artist = a.name;
+                            return song;
+                        });
+                        this.songs = songs;
+                    });
+            },
             fetchVotes(songs) {
                 // Get all votes
                 const url = "http://webservies.be/eurosong/Votes";
@@ -63,15 +83,24 @@
                     })
                     .then((votes) => {
                         // loop over array songs with map method and then add totalPoints to songs with the sum of all vote points
+                        let allPoints = 0;
                         songs.map(song => {
                             // create totalPoints variable to get the sum of all vote points
                             song.totalPoints = 0;
                             //filter all the votes out where the vote songID is the same as the song id, because there are multiple votes for 1 song, then add up all points for 1 song and add it to that song's object in the JSON (totalPoints)
                             votes.filter((vote) => vote.songID == song.id).forEach(element => {
-                                if (element.songID == song.id) song.totalPoints += element.points
+                                if (element.songID == song.id) {
+                                    song.totalPoints += element.points
+                                    allPoints += element.points
+                                    }
                             });
                             return song;
                         });
+
+                        songs.forEach(song => {
+                            song.percentage = Number((song.totalPoints / allPoints * 100).toFixed(2));
+                            console.log(song.percentage)
+                        })  
                         // sort songs from best to worst and change data of songs, so everything will get rerenderd;
                         this.songs = songs.sort((a, b) => b.totalPoints - a.totalPoints );
                     });
